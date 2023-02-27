4afa17e8f13f0aab4be2249a4304a51bb61986ca
mbrsagor <mbrsagor@gmail.com>
2023-02-27 23:05:57 +0600

    New: inital new commit.

--- blog/main.py
+++ blog/main.py
@@ -12,3 +12,7 @@ models.Base.metadata.create_all(engine)
 @app.post("/blog")
 async def create(data: schemas.Blog, db: Session):
     return db
+
+"""
+https://www.youtube.com/watch?v=7t2alSnE2-I&list=PLe30vg_FG4OSKH_8zpLlnf4WpNlzL526E&ab_channel=Bitfumes
+"""
